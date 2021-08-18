46d184d93e4e948e87bdc44351407802cc2c27f8
bUSHwACKEr82 <89160018+bUSHwACKEr82@users.noreply.github.com>
2021-08-18 20:05:33 +0100

    Add files via upload

---
+++ Login_o365.ps1
@@ -0,0 +1,3 @@
+﻿$Cred = Get-Credential -credential dcowley@asmtech.com
+$s = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $cred -Authentication Basic –AllowRedirection
+Import-PSSession $s
